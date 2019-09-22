2cf209b5aef145e54e687e11d91af3d5f35423c1
deedeeh <dinahafez.8@gmail.com>
2019-09-22 22:54:17 +0100

    Made simple tests to fail then pass for one rule without refactoring

---
+++ first_practice/index.test.js
@@ -0,0 +1,25 @@
+const index = require('./index');
+
+test('when year 1991 return false', () => {
+  expect(index(1991)).toBe(false);
+}); 
+
+test('when year 1993 return false', () => {
+  expect(index(1993)).toBe(false);
+});
+
+test('when year 1997 return false', () => {
+  expect(index(1997)).toBe(false);
+});
+
+test('if 1992 return true', () => {
+  expect(index(1992)).toBe(true);
+});
+
+test('if 1996 return true', () => {
+  expect(index(1996)).toBe(true);
+});
+
+test('if 2000 return true', () => {
+  expect(index(2000)).toBe(true);
+});
