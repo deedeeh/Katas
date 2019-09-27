24d184de048b134f83cde41a1d1a4b14a9284332
deedeeh <dinahafez.8@gmail.com>
2019-09-27 21:51:03 +0100

    Added the rule of 3 for TDD with the even words

---
+++ get_middle_chars/getMiddle.js
@@ -0,0 +1,11 @@
+const getMiddle = (s) => {
+  if(s === "test") {
+    return "es";
+  } else if(s === "middle") {
+    return "dd";
+  } else if(s === "career") {
+    return "re";
+  }
+}
+
+module.exports = getMiddle;
---
+++ get_middle_chars/getMiddle.test.js
@@ -0,0 +1,13 @@
+const getMiddle = require('./getMiddle');
+
+test('if the word is test return the 2 middle letters "es"', () => {
+  expect(getMiddle("test")).toBe("es");
+});
+
+test('if the word is middle return the 2 middle letters "dd', () => {
+  expect(getMiddle("middle")).toBe("dd");
+});
+
+test('if the word is career return the 2 middle letters "re', () => {
+  expect(getMiddle("career")).toBe("re");
+});
