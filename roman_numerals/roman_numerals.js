37219da035f966cd0dd6edbdabf329e8425b813a
deedeeh <dinahafez.8@gmail.com>
2019-10-12 22:41:25 +0100

    Worked on roman numerals with TDD following a tutorial on youtube

--- roman_numerals/roman_numerals.js
+++ roman_numerals/roman_numerals.js
@@ -1,11 +1,27 @@
 const convert = (number) => {
-  if(number === 1) {
-    return "I"
-  } else if(number === 2) {
-    return "II"
-  } else if(number === 3) {
-    return "III"
+  let result = '';
+  const numeralsObj = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
   }
+  for(let key in numeralsObj) {
+    if(number >= numeralsObj[key]) {
+      result += key.repeat(Math.trunc(number / numeralsObj[key]))
+      number -= numeralsObj[key] * Math.trunc(number / numeralsObj[key])
+    }
+  }
+  return result;
 }
 
 module.exports = convert;
--- roman_numerals/roman_numerals.test.js
+++ roman_numerals/roman_numerals.test.js
@@ -4,11 +4,41 @@ test('if 1 return "I"', () => {
   expect(convert(1)).toBe("I");
 });
 
-test('if 2 return "II', () => {
+test('if 2 return "II"', () => {
   expect(convert(2)).toBe("II");
 });
 
-test('if 3 return "III', () => {
+test('if 3 return "III"', () => {
   expect(convert(3)).toBe("III");
 });
+
+test('if 4 return "IV"', () => {
+  expect(convert(4)).toBe("IV");
+});
+
+test('if 9 return "IX"', () => {
+  expect(convert(9)).toBe("IX");
+});
+
+test('if 29 return "XXIX"', () => {
+  expect(convert(29)).toBe("XXIX");
+});
+
+test('if 80 return "LXXX"', () => {
+  expect(convert(80)).toBe("LXXX");
+});
+
+test('if 294 return "CCXCIV"', () => {
+  expect(convert(294)).toBe("CCXCIV");
+});
+
+test('if 2019 return "MMXIX"', () => {
+  expect(convert(2019)).toBe("MMXIX");
+});
+
+
+
+
+
+
 
