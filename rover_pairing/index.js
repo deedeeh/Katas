e99c0347bd13c9f9465c5faf4ef674ff335c2d74
deedeeh <dinahafez.8@gmail.com>
2019-10-13 00:40:30 +0100

    Worked on the right or left command with just 1 move. Next step more refactoring

--- rover_pairing/index.js
+++ rover_pairing/index.js
@@ -1,18 +1,33 @@
 const rover = (x, y, direction, commands) => {
-  let newXCoordinate;
-  let newYCoordinate; 
+  let newXCoordinate = x;
+  let newYCoordinate = y; 
+  if(direction === "E" && commands === "L" || direction === "W" && commands === "L") {
+    newYCoordinate = y + commands.length
+    return `${newXCoordinate}:${newYCoordinate}:${direction}`
+  } else if(direction === "E" && commands === "R" || direction === "W" && commands === "R") {
+    newYCoordinate = y - commands.length
+    return `${newXCoordinate}:${newYCoordinate}:${direction}`
+  }
+  if(direction === "N" && commands === "L" || direction === "S" && commands === "L") {
+    newXCoordinate = x - commands.length
+    return `${newXCoordinate}:${newYCoordinate}:${direction}`
+  } else if(direction === "N" && commands === "R" || direction === "S" && commands === "R") {
+    newXCoordinate = x + commands.length
+    return `${newXCoordinate}:${newYCoordinate}:${direction}`
+  }
+
   if(direction === "N") {
     newYCoordinate = y + commands.length
-    return `${x}:${newYCoordinate}:${direction}`
+    return `${newXCoordinate}:${newYCoordinate}:${direction}`
   } else if(direction === "E") {
     newXCoordinate = x + commands.length
-    return `${newXCoordinate}:${y}:${direction}`
+    return `${newXCoordinate}:${newYCoordinate}:${direction}`
   } else if(direction === "W") {
     newXCoordinate = x - commands.length
-    return `${newXCoordinate}:${y}:${direction}`
+    return `${newXCoordinate}:${newYCoordinate}:${direction}`
   }  else if(direction === "S") {
     newYCoordinate = y - commands.length
-    return `${x}:${newYCoordinate}:${direction}`
+    return `${newXCoordinate}:${newYCoordinate}:${direction}`
   }
 }
 
--- rover_pairing/index.test.js
+++ rover_pairing/index.test.js
@@ -1,3 +1,6 @@
+//"M" represents 1 step forward 
+//"L" for left and "R" right
+
 const rover = require('./index');
 
 test('rover moves 1 step east', () => {
@@ -47,6 +50,82 @@ test('rover moves 2 steps south', () => {
 test('rover moves 9 steps south', () => {
   expect(rover(2, 6, "S", "MMMMMMMMM")).toBe("2:-3:S")
 });
+
+test('rover turns left from the east', () => {
+  expect(rover(0, 0, "E", "L")).toBe("0:1:E")
+}); 
+
+test('rover turns left from the east', () => {
+  expect(rover(3, 2, "E", "L")).toBe("3:3:E")
+}); 
+
+test('rover turns left from the east', () => {
+  expect(rover(5, 7, "E", "L")).toBe("5:8:E")
+}); 
+
+test('rover turns right from the east', () => {
+  expect(rover(0, 0, "E", "R")).toBe("0:-1:E")
+}); 
+
+test('rover turns right from the east', () => {
+  expect(rover(10, 6, "E", "R")).toBe("10:5:E")
+}); 
+
+test('rover turns left from the west', () => {
+  expect(rover(4, 4, "W", "L")).toBe("4:5:W")
+}); 
+
+test('rover turns left from the west', () => {
+  expect(rover(2, 7, "W", "L")).toBe("2:8:W")
+}); 
+
+test('rover turns right from the west', () => {
+  expect(rover(2, 7, "W", "R")).toBe("2:6:W")
+}); 
+
+test('rover turns right from the west', () => {
+  expect(rover(10, 1, "W", "R")).toBe("10:0:W")
+}); 
+
+test('rover turns left from the north', () => {
+  expect(rover(3, 3, "N", "L")).toBe("2:3:N")
+}); 
+
+test('rover turns left from the north', () => {
+  expect(rover(9, 2, "N", "L")).toBe("8:2:N")
+}); 
+
+test('rover turns right from the north', () => {
+  expect(rover(9, 2, "N", "R")).toBe("10:2:N")
+}); 
+
+test('rover turns right from the north', () => {
+  expect(rover(5, 2, "N", "R")).toBe("6:2:N")
+}); 
+
+test('rover turns left from the south', () => {
+  expect(rover(8, 2, "S", "L")).toBe("7:2:S")
+}); 
+
+test('rover turns left from the south', () => {
+  expect(rover(2, 4, "S", "L")).toBe("1:4:S")
+}); 
+
+test('rover turns right from the south', () => {
+  expect(rover(2, 4, "S", "R")).toBe("3:4:S")
+}); 
+
+test('rover turns right from the south', () => {
+  expect(rover(7, 4, "S", "R")).toBe("8:4:S")
+}); 
+
+
+
+
+
+
+
+
 
 
 
