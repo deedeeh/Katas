30e6b4e8d08e2acacb8b63531b4d435a79f8f328
deedeeh <dinahafez.8@gmail.com>
2019-10-13 23:13:59 +0100

    Updated the files to the correct code for R & L commands

--- rover_pairing/index.js
+++ rover_pairing/index.js
@@ -2,33 +2,25 @@ const rover = (x, y, direction, commands) => {
   let newXCoordinate = x;
   let newYCoordinate = y; 
   if(direction === "E" && commands === "L" || direction === "W" && commands === "L") {
-    newYCoordinate = y + commands.length
-    return `${newXCoordinate}:${newYCoordinate}:${direction}`
+    return `${newXCoordinate}:${newYCoordinate}:N`
   } else if(direction === "E" && commands === "R" || direction === "W" && commands === "R") {
-    newYCoordinate = y - commands.length
-    return `${newXCoordinate}:${newYCoordinate}:${direction}`
-  }
-  if(direction === "N" && commands === "L" || direction === "S" && commands === "L") {
-    newXCoordinate = x - commands.length
-    return `${newXCoordinate}:${newYCoordinate}:${direction}`
+    return `${newXCoordinate}:${newYCoordinate}:S`
+  } else if(direction === "N" && commands === "L" || direction === "S" && commands === "L") {
+    return `${newXCoordinate}:${newYCoordinate}:W`
   } else if(direction === "N" && commands === "R" || direction === "S" && commands === "R") {
-    newXCoordinate = x + commands.length
-    return `${newXCoordinate}:${newYCoordinate}:${direction}`
+    return `${newXCoordinate}:${newYCoordinate}:E`
   }
 
   if(direction === "N") {
     newYCoordinate = y + commands.length
-    return `${newXCoordinate}:${newYCoordinate}:${direction}`
   } else if(direction === "E") {
     newXCoordinate = x + commands.length
-    return `${newXCoordinate}:${newYCoordinate}:${direction}`
   } else if(direction === "W") {
     newXCoordinate = x - commands.length
-    return `${newXCoordinate}:${newYCoordinate}:${direction}`
   }  else if(direction === "S") {
     newYCoordinate = y - commands.length
-    return `${newXCoordinate}:${newYCoordinate}:${direction}`
   }
+  return `${newXCoordinate}:${newYCoordinate}:${direction}`
 }
 
 module.exports = rover
--- rover_pairing/index.test.js
+++ rover_pairing/index.test.js
@@ -51,72 +51,84 @@ test('rover moves 9 steps south', () => {
   expect(rover(2, 6, "S", "MMMMMMMMM")).toBe("2:-3:S")
 });
 
-test('rover turns left from the east', () => {
-  expect(rover(0, 0, "E", "L")).toBe("0:1:E")
+test('rover\' direction is east and it turns left', () => {
+  expect(rover(0, 0, "E", "L")).toBe("0:0:N")
 }); 
 
-test('rover turns left from the east', () => {
-  expect(rover(3, 2, "E", "L")).toBe("3:3:E")
+test('rover\' direction is east and it turns left', () => {
+  expect(rover(3, 2, "E", "L")).toBe("3:2:N")
 }); 
 
-test('rover turns left from the east', () => {
-  expect(rover(5, 7, "E", "L")).toBe("5:8:E")
+test('rover\' direction is east and it turns left', () => {
+  expect(rover(5, 7, "E", "L")).toBe("5:7:N")
 }); 
 
-test('rover turns right from the east', () => {
-  expect(rover(0, 0, "E", "R")).toBe("0:-1:E")
+test('rover\' direction is east and it turns right', () => {
+  expect(rover(2, 1, "E", "R")).toBe("2:1:S")
 }); 
 
-test('rover turns right from the east', () => {
-  expect(rover(10, 6, "E", "R")).toBe("10:5:E")
+test('rover\' direction is east and it turns right', () => {
+  expect(rover(0, 0, "E", "R")).toBe("0:0:S")
 }); 
 
-test('rover turns left from the west', () => {
-  expect(rover(4, 4, "W", "L")).toBe("4:5:W")
+test('rover\' direction is east and it turns right', () => {
+  expect(rover(10, 6, "E", "R")).toBe("10:6:S")
 }); 
 
-test('rover turns left from the west', () => {
-  expect(rover(2, 7, "W", "L")).toBe("2:8:W")
+test('rover\' direction is west and it turns left', () => {
+  expect(rover(1, 1, "W", "L")).toBe("1:1:N")
 }); 
 
-test('rover turns right from the west', () => {
-  expect(rover(2, 7, "W", "R")).toBe("2:6:W")
+test('rover\' direction is west and it turns left', () => {
+  expect(rover(4, 4, "W", "L")).toBe("4:4:N")
 }); 
 
-test('rover turns right from the west', () => {
-  expect(rover(10, 1, "W", "R")).toBe("10:0:W")
+test('rover\' direction is west and it turns left', () => {
+  expect(rover(2, 7, "W", "L")).toBe("2:7:N")
 }); 
 
-test('rover turns left from the north', () => {
-  expect(rover(3, 3, "N", "L")).toBe("2:3:N")
+test('rover\' direction is west and it turns right', () => {
+  expect(rover(5, 5, "W", "R")).toBe("5:5:S")
 }); 
 
-test('rover turns left from the north', () => {
-  expect(rover(9, 2, "N", "L")).toBe("8:2:N")
+test('rover\' direction is west and it turns right', () => {
+  expect(rover(2, 7, "W", "R")).toBe("2:7:S")
 }); 
 
-test('rover turns right from the north', () => {
-  expect(rover(9, 2, "N", "R")).toBe("10:2:N")
+test('rover\' direction is west and it turns right', () => {
+  expect(rover(9, 1, "W", "R")).toBe("9:1:S")
 }); 
 
-test('rover turns right from the north', () => {
-  expect(rover(5, 2, "N", "R")).toBe("6:2:N")
+test('rover\' direction is north and it turns left', () => {
+  expect(rover(3, 3, "N", "L")).toBe("3:3:W")
 }); 
 
-test('rover turns left from the south', () => {
-  expect(rover(8, 2, "S", "L")).toBe("7:2:S")
+test('rover\' direction is north and it turns left', () => {
+  expect(rover(9, 2, "N", "L")).toBe("9:2:W")
 }); 
 
-test('rover turns left from the south', () => {
-  expect(rover(2, 4, "S", "L")).toBe("1:4:S")
+test('rover\' direction is north and it turns right', () => {
+  expect(rover(8, 2, "N", "R")).toBe("8:2:E")
 }); 
 
-test('rover turns right from the south', () => {
-  expect(rover(2, 4, "S", "R")).toBe("3:4:S")
+test('rover\' direction is north and it turns right', () => {
+  expect(rover(5, 2, "N", "R")).toBe("5:2:E")
 }); 
 
-test('rover turns right from the south', () => {
-  expect(rover(7, 4, "S", "R")).toBe("8:4:S")
+test('rover\' direction is south and it turns left', () => {
+  expect(rover(8, 2, "S", "L")).toBe("8:2:W")
+}); 
+
+test('rover\' direction is south and it turns left', () => {
+  expect(rover(2, 4, "S", "L")).toBe("2:4:W")
+}); 
+
+test('rover\' direction is south and it turns right', () => {
+  expect(rover(2, 4, "S", "R")).toBe("2:4:E")
+}); 
+
+test('rover\' direction is south and it turns right', () => {
+  expect(rover(7, 4, "S", "R")).toBe("7:4:E")
 }); 
 
 
