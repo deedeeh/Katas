6016842aaaf1d25f695e0b723c875d138b3ca566
deedeeh <dinahafez.8@gmail.com>
2019-10-15 22:02:47 +0100

    Tested with the rule of 3 for the multiple of 5 numbers. no refactor yet

--- fizzBuzz_TDD/fizzBuzz.js
+++ fizzBuzz_TDD/fizzBuzz.js
@@ -1,4 +1,11 @@
 const fizzBuzz = (num) => {
+  if(num === 5) {
+    return "Buzz"
+  } else if(num === 10) {
+    return "Buzz"
+  } else if(num === 15) {
+    return "Buzz"
+  }
   if(num % 3 === 0) {
     return "Fizz"
   }
--- fizzBuzz_TDD/fizzBuzz.test.js
+++ fizzBuzz_TDD/fizzBuzz.test.js
@@ -15,3 +15,15 @@ test('number 9 should return "Fizz"', () => {
 test('if number is multiple of 3 return "Fizz"', () => {
   expect(fizzBuzz(324)).toBe("Fizz")
 });
+
+test('number 5 should return "Buzz', () => {
+  expect(fizzBuzz(5)).toBe("Buzz")
+});
+
+test('number 10 should return "Buzz', () => {
+  expect(fizzBuzz(10)).toBe("Buzz")
+});
+
+test('number 15 should return "Buzz', () => {
+  expect(fizzBuzz(15)).toBe("Buzz")
+});
